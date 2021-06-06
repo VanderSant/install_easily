3bfccbc81c2916669738fce3e31ef8acce8e1340
VanderSant <vanderson.santos@thunderatz.org>
2021-06-06 05:02:02 -0300

    🔨  add update_gazebo_newest_9_version

--- update/update_gazebo_newest_9_version.sh
+++ update/update_gazebo_newest_9_version.sh
@@ -1,15 +1,14 @@
 #!/usr/bin/env bash
 
+# Update lib math2
 sudo apt-get update
-
 sudo apt upgrade libignition-math2 -y
 
+# Update gazebo9 to the newest version 
 sudo sh -c 'echo "deb http://packages.osrfoundation.org/gazebo/ubuntu-stable `lsb_release -cs` main" > /etc/apt/sources.list.d/gazebo-stable.list'
-
 wget https://packages.osrfoundation.org/gazebo.key -O - | sudo apt-key add -
-
 sudo apt-get update
-
 sudo apt-get install gazebo9 -y
 
+# show new version
 gazebo -version

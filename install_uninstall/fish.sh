2901be7269a6ca5d3a71faad959600db1e62bcf0
VanderSant <vanderson.santos@usp.br>
2021-10-29 02:12:10 -0300

    🚧  add fish installer

---
+++ install_uninstall/fish.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+# Purpose: Install or Uninstall Fish
+# Author: 
+# Note : The script must run as a cron-job.
+# Last updated on : 09/2021
+# -----------------------------------------------
+
+echo Hello, I am will install or uninstall fish!
+read -p 'What do you want? install(i) uninstall(u) ' mode
+
+if test "$mode" = "i"
+then
+    # Update before starting
+    sudo apt update
+    # Install fish
+    sudo apt-get install fish -y 
+    # Show fish version
+    fish -v
+elif test "$mode" = "u"
+then
+    # Uninstall fish
+    sudo apt-get autoremove fish -y
+    sudo apt-get autoremove --purge fish -y
+else
+    echo "not a valid option"
+fi
---
+++ terminal.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# Update and upgrade before starting
+#sudo apt update
+#sudo apt upgrade -y
+
+# Install fish
+./install_uninstall/fish.sh 

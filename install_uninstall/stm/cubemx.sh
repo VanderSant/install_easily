ad5c8b8bb8714eea139c9c5faaa737e2aac0e122
VanderSant <vanderson.santos@enline-transmission.com>
2022-08-14 23:12:38 -0300

    🔨 update stm32 cube installations scripts

--- install_uninstall/stm/cubemx.sh
+++ install_uninstall/stm/cubemx.sh
@@ -1,18 +1,33 @@
 #!/bin/bash
 
+# EMAIL=vanderson0001@gmail.com
+# SENHA=Sinequanon@153047
+
+# setuṕ
 ROOT_PATH=$(pwd)
 INSTALATION_PATH=$ROOT_PATH/instalations
 CUBE_PATH=$INSTALATION_PATH/cubemx
 
+# script
 sudo apt purge openjfx
 sudo apt install openjfx=8u161-b12-1ubuntu2 libopenjfx-jni=8u161-b12-1ubuntu2 libopenjfx-java=8u161-b12-1ubuntu2
 sudo apt-mark hold openjfx libopenjfx-jni libopenjfx-java
 
 cd instalations
 mkdir stm_cube_mx && cd stm_cube_mx
-curl -O https://sw-center.st.com/packs/resource/library/stm32cube_mx_v650-lin.zip
-unzip stm32cube_mx_v650-lin.zip
+mkdir install
+curl -O https://www.st.com/content/ccc/resource/technical/software/sw_development_suite/group0/7b/42/34/97/63/b7/4d/05/stm32cubemx-lin_v6-6-1/files/stm32cubemx-lin_v6-6-1.zip/jcr:content/translations/en.stm32cubemx-lin_v6-6-1.zip
+unzip -q en.stm32cubemx-lin_v6-6-1.zip 
 
+chmod +x SetupSTM32CubeMX-6.6.1
+./SetupSTM32CubeMX-6.6.1 
+
+# path
+echo "export CUBE_PATH=$ROOT_PATH/instalations/stm_cube_mx/install" >> ~/.bashrc
 
 source ~/.bashrc
 cd $ROOT_PATH
+
+#                   /content/ccc/resource/technical/software/sw_development_suite/group0/7b/42/34/97/63/b7/4d/05/stm32cubemx-lin_v6-6-1/files/stm32cubemx-lin_v6-6-1.zip/jcr:content/translations/en.stm32cubemx-lin_v6-6-1.zip
+# https://www.st.com/content/ccc/resource/technical/software/sw_development_suite/group0/7b/42/34/97/63/b7/4d/05/stm32cubemx-lin_v6-6-1/files/stm32cubemx-lin_v6-6-1.zip/_jcr_content/translations/en.stm32cubemx-lin_v6-6-1.zip
+# https://www.st.com/content/ccc/resource/technical/software/sw_development_suite/group0/7b/42/34/97/63/b7/4d/05/stm32cubemx-lin_v6-6-1/files/stm32cubemx-lin_v6-6-1.zip/jcr:content/translations/en.stm32cubemx-lin_v6-6-1.zip
--- install_uninstall/stm/cubeprog.sh
+++ install_uninstall/stm/cubeprog.sh
@@ -6,9 +6,20 @@ CUBE_PATH=$INSTALATION_PATH/cubemx
 
 cd instalations
 mkdir stm_cube_programmer && cd stm_cube_programmer
-curl -O https://www.st.com/content/ccc/resource/technical/software/utility/group0/2b/58/90/97/ad/a1/46/10/stm32cubeprg-lin_v2-10-0/files/stm32cubeprg-lin_v2-10-0.zip/jcr:content/translations/en.stm32cubeprg-lin_v2-10-0.zip
-unzip stm32cubeprg-lin_v2-10-0.zip
+mkdir install
+curl -O https://www.st.com/content/ccc/resource/technical/software/utility/group0/10/04/c7/eb/76/2e/40/9c/stm32cubeprg-lin_v2-11-0/files/stm32cubeprg-lin_v2-11-0.zip/jcr:content/translations/en.stm32cubeprg-lin_v2-11-0.zip
+unzip en.stm32cubeprg-lin_v2-11-0.zip
 
+chmod +x SetupSTM32CubeProgrammer-2.11.0.linux
+./SetupSTM32CubeProgrammer-2.11.0.linux 
+
+# path
+echo "export PATH=\$PATH:$ROOT_PATH/instalations/stm_cube_programmer/install/bin" >> ~/.bashrc
 
 source ~/.bashrc
 cd $ROOT_PATH
+
+# https://www.st.com/content/ccc/resource/technical/software/utility/group0/2b/58/90/97/ad/a1/46/10/stm32cubeprg-lin_v2-10-0/files/stm32cubeprg-lin_v2-10-0.zip/jcr:content/translations/en.stm32cubeprg-lin_v2-10-0.zip
+#                   /content/ccc/resource/technical/software/utility/group0/10/04/c7/eb/76/2e/40/9c/stm32cubeprg-lin_v2-11-0/files/stm32cubeprg-lin_v2-11-0.zip/jcr:content/translations/en.stm32cubeprg-lin_v2-11-0.zip
+#                   /content/ccc/resource/technical/software/utility/group0/10/04/c7/eb/76/2e/40/9c/stm32cubeprg-lin_v2-11-0/files/stm32cubeprg-lin_v2-11-0.zip/jcr:content/translations/en.stm32cubeprg-lin_v2-11-0.zip
+# https://www.st.com/content/ccc/resource/technical/software/utility/group0/10/04/c7/eb/76/2e/40/9c/stm32cubeprg-lin_v2-11-0/files/stm32cubeprg-lin_v2-11-0.zip/jcr:content/translations/en.stm32cubeprg-lin_v2-11-0.zip
